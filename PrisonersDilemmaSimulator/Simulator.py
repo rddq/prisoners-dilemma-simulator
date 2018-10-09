d3a7ccd0909d02c9e185f2f61698b6808b3e3a4d
Ryan Day <ryan.david.day@gmail.com>
2018-10-09 16:22:08 -0600

    Updated Analysis

--- PrisonersDilemmaSimulator/Simulator.py
+++ PrisonersDilemmaSimulator/Simulator.py
@@ -2,7 +2,7 @@ from Game import Game
 from Player import Player
 from AnalysisHelper import AnalysisHelper
 
-import numpy
+import pandas
 
 class Simulator:
     
@@ -10,6 +10,30 @@ class Simulator:
         pass
     
 if __name__ == '__main__':   
+    names_of_strategies = {
+        0 : "Two tits for a tat",
+        1 : "Always defect",
+        2 : "Random",
+        3 : "Always Cooperate",
+        4 : "Tit for a tat",
+        5 : "Tit for two tats",
+        6 : "Pavlov_strategy",
+        7 : "Win stay lose shift",
+        8 : "Never forgive"
+    }
+
+    game_type = {
+        0 : "5 games",
+        1 : "Always defect",
+        2 : "Random",
+        3 : "Always Cooperate",
+        4 : "Tit for a tat",
+        5 : "Tit for two tats",
+        6 : "Pavlov_strategy",
+        7 : "Win stay lose shift",
+        8 : "Never forgive"
+    }
+    
     all_results = []
     data_for_csv = []
     for player_1_type in range(0,9):
@@ -17,18 +41,26 @@ if __name__ == '__main__':
             players = [Player(player_1_type,player_number=0),Player(player_2_type,player_number=1)]   
             game = Game(players)
             results_5 = game.playGame(5)
-            AnalysisHelper.add_to_csv(data_for_csv,results_5,game,player_1_type,player_2_type)
+            player_1_strat = names_of_strategies.get(player_1_type)
+            player_2_strat = names_of_strategies.get(player_2_type)
+
+            AnalysisHelper.add_to_csv(data_for_csv,results_5,game,player_1_strat,player_2_strat,"5 games")
+            
             results_100 = game.playGame(100)
-            AnalysisHelper.add_to_csv(data_for_csv,results_100,game,player_1_type,player_2_type)
+            AnalysisHelper.add_to_csv(data_for_csv,results_100,game,player_1_strat,player_2_strat,"100 games")
+            
             results_200 = game.playGame(200)
-            AnalysisHelper.add_to_csv(data_for_csv,results_200,game,player_1_type,player_2_type)
+            AnalysisHelper.add_to_csv(data_for_csv,results_200,game,player_1_strat,player_2_strat, "200 games")
             for k in range(0,30):
                 results_p75 = game.playGame(10000, 0.75)
-                AnalysisHelper.add_to_csv(data_for_csv,results_p75,game,player_1_type,player_2_type)
+                AnalysisHelper.add_to_csv(data_for_csv,results_p75,game,player_1_strat,player_2_strat, "Beta = 0.75")
+                
                 results_p90 = game.playGame(10000, 0.9)
-                AnalysisHelper.add_to_csv(data_for_csv,results_p90,game,player_1_type,player_2_type)
+                AnalysisHelper.add_to_csv(data_for_csv,results_p90,game,player_1_strat,player_2_strat, "Beta = 0.9")
+                
                 results_p99 = game.playGame(10000, 0.99)
-                AnalysisHelper.add_to_csv(data_for_csv,results_p99,game,player_1_type,player_2_type)
-    data = numpy.asarray(data_for_csv)
-    numpy.savetxt("PrisonersDilemmaMatchData.csv", data, delimiter=",")
+                AnalysisHelper.add_to_csv(data_for_csv,results_p99,game,player_1_strat,player_2_strat, "Beta = 0.99")
+    
+    data = pandas.DataFrame.from_records(data_for_csv) 
+    pandas.DataFrame.to_csv("PrisonersDilemmaMatchData.csv", sep=",")
         
